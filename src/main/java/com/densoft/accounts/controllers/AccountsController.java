2713c1ddff1d9da11282a5871c2828f732b2192f
dennis githinji <wakahiad@gmail.com>
2022-11-21 19:43:11 +0300

    connection to config server

--- src/main/java/com/densoft/accounts/controllers/AccountsController.java
+++ src/main/java/com/densoft/accounts/controllers/AccountsController.java
@@ -1,24 +1,43 @@
 package com.densoft.accounts.controllers;
 
+import com.densoft.accounts.config.AccountsConfig;
 import com.densoft.accounts.model.Accounts;
 import com.densoft.accounts.model.Customer;
+import com.densoft.accounts.model.Properties;
 import com.densoft.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    private AccountsConfig accountsConfig;
+
     @PostMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestBody Customer customer){
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-        if(accounts != null){
+        if (accounts != null) {
             return accounts;
         }
         return null;
+    }
+
+    @GetMapping("/account/properties")
+    public String getProperties() throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(), accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        return objectWriter.writeValueAsString(properties);
     }
 }
