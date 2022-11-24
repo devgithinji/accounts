1b8c01a073ea0174399f71779eb7f5de836ca6aa
dennis githinji <wakahiad@gmail.com>
2022-11-24 16:17:55 +0300

    resilience 4j config and rate limitter pattern with fallback method setup

--- src/main/java/com/densoft/accounts/controllers/AccountsController.java
+++ src/main/java/com/densoft/accounts/controllers/AccountsController.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,13 +52,26 @@ public class AccountsController {
 
     @PostMapping("/myCustomerDetails")
 //    @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
-    @Retry(name = "retryForCustomerDetails",fallbackMethod = "myCustomerDetailsFallBack")
+    @Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
     public CustomerDetails customerDetails(@RequestBody Customer customer) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         List<Cards> cardsList = cardsFeignClient.getCardDetails(customer);
         List<Loans> loansList = loansFeignClient.getLoansDetails(customer);
         return new CustomerDetails(accounts, loansList, cardsList);
     }
+
+
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallBack")
+    public String sayHello() {
+        return "Hello, Welcome to Eazy Bank";
+    }
+
+    private String sayHelloFallBack(){
+        return "Hi, Welcome to Eazy Bank";
+    }
+
+
 
     private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable throwable) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
