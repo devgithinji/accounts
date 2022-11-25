8d9e89c80d077eeeb5aec92d52bab92740b3a755
dennis githinji <wakahiad@gmail.com>
2022-11-25 18:45:16 +0300

    spring sleuth zipkin and spring-rabbit setup

--- src/main/java/com/densoft/accounts/controllers/AccountsController.java
+++ src/main/java/com/densoft/accounts/controllers/AccountsController.java
@@ -11,6 +11,8 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,8 @@ import java.util.List;
 
 @RestController
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
     @Autowired
     private AccountsRepository accountsRepository;
 
@@ -51,9 +55,11 @@ public class AccountsController {
     @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
     @Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
     public CustomerDetails customerDetails(@RequestHeader("eazybank-correlation-id") String correlationId, @RequestBody Customer customer) {
+        logger.info("myCustomerDetails() method started");
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         List<Cards> cardsList = cardsFeignClient.getCardDetails(correlationId, customer);
         List<Loans> loansList = loansFeignClient.getLoansDetails(correlationId, customer);
+        logger.info("myCustomerDetails() method ended");
         return new CustomerDetails(accounts, loansList, cardsList);
     }
 
